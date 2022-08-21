10338f74181f7888142673152a1183d719471a15
Franck Allyson <franckallyson@hotmail.com>
2022-08-20 21:29:40 -0300

    Implementação da fila encadeada, passível de mudanças

--- codigo_fonte/utilidades/imports.py
+++ codigo_fonte/utilidades/imports.py
@@ -9,7 +9,7 @@ __email__ = "caio.lamas@ifnmg.edu,br"
 from codigo_fonte.estruturas_de_dados.lista_encadeada import lista_encadeada
 from codigo_fonte.estruturas_de_dados.lista_duplamente_encadeada import lista_duplamente_encadeada
 from codigo_fonte.estruturas_de_dados.pilha_encadeada import pilha_encadeada
-
+from codigo_fonte.estruturas_de_dados.fila_encadeada import fila_encadeada
 
 def executar(func):
     """ Executa o teste/exemplo inserido na lista de parâmetros.
