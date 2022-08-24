e4a52708e5c834d7415ade381de61822a3a76fe2
doYourCode <caio.lamas@ifnmg.edu.br>
2022-08-24 18:07:17 -0300

    Refs

--- codigo_fonte/estruturas_de_dados/arvore_binaria_de_busca.py
+++ codigo_fonte/estruturas_de_dados/arvore_binaria_de_busca.py
@@ -1,1 +1,2 @@
 # TODO
+# https://www.educba.com/types-of-trees-in-data-structure/
--- codigo_fonte/estruturas_de_dados/tabela_hash.py
+++ codigo_fonte/estruturas_de_dados/tabela_hash.py
@@ -1,3 +1,4 @@
 # TODO
 
 # https://www.educba.com/hash-table-in-data-structure/?source=leftnav
+# https://www.youtube.com/results?search_query=python+hash+table
