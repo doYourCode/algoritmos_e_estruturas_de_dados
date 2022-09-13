c619287e7574e67070d3676ebba533df9f04473b
Everton Sousa Oliveira <evertonsousa3557@gmail.com>
2022-09-13 19:58:20 -0300

    Implementação de Fila e refatoraçãode pilha

--- codigo_fonte/estruturas_de_dados/fila.py
+++ codigo_fonte/estruturas_de_dados/fila.py
@@ -1,1 +1,95 @@
-# TODO
+__author__ = ["Caio Henriques Sica Lamas",
+              "Everton Sousa Oliveira"]
+__date__ = "13/09/2022"
+__credits__ = ["https://www.techiedelight.com/pt/filaTesteueue-implementation-python/"]
+__license__ = "GPL"
+__email__ = "eso@aluno.ifnmg.edu.br, caio.lamas@ifnmg.edu.br"
+
+from codigo_fonte.utilidades.utilidades import *
+
+# Implementação de fila em Python
+class Fila:
+    """Inicializar fila"""
+    def __init__(self, tamanho):
+        self.valor = None
+        self.filaTeste = [None] * tamanho
+        self.capacidade = tamanho
+        self.frente = 0
+        self.final = -1
+        self.contador = 0
+
+    """Função para remover o elemento da frente"""
+    def remover(self):
+        """ Verifica se a fila está vazia"""
+        if self.estaVazia():
+            print('Fila Vazia! Encerrando processo...')
+            exit(-1)
+        x = self.filaTeste[self.frente]
+        print('Removendo elemento...', x)
+        self.frente = (self.frente + 1) % self.capacidade
+        self.contador = self.contador - 1
+        return x
+
+    """Função para adicionar um elemento à fila"""
+    def enfileirar(self, valor):
+        """verifica se há estouro de fila"""
+        if self.estaCheia():
+            print('Fila Cheia! Encerrando processo...')
+            exit(-1)
+        print('Inserindo elemento...', valor)
+        self.final = (self.final + 1) % self.capacidade
+        self.filaTeste[self.final] = valor
+        self.contador = self.contador + 1
+
+    """Função para retornar o elemento da frente da fila sem apagar"""
+    def peek(self):
+        if self.estaVazia():
+            print('Fila Vazia! Encerrando processo...')
+            exit(-1)
+        return self.filaTeste[self.frente]
+
+    """Função para retornar o tamanho da fila"""
+    def tamanho(self):
+        return self.contador
+
+    """Função para verificar se a fila está vazia ou não"""
+    def estaVazia(self):
+        return self.tamanho() == 0
+
+    """Função para verificar se a fila está cheia ou não"""
+    def estaCheia(self):
+        return self.tamanho() == self.capacidade
+
+
+if __name__ == '__main__':
+
+    filaTeste = Fila(5)  # ............ Define o tamanho da fila
+    # Testando inserção
+    filaTeste.enfileirar(1)  # ............ Insere o primeiro elemento
+    filaTeste.enfileirar(2)  # ............ Insere o segundo elemento
+    filaTeste.enfileirar(3)  # ............ Insere o terceiro elemento
+    print(f'{HEADER}Teste Nº 1 (Inserções): {ENDC}')
+    print(f'O tamanho da fila é: {filaTeste.tamanho()}')
+    print(f'O elemento da frente é: {filaTeste.peek()}')
+
+    # Testando a remoção
+    filaTeste.remover()  # ............ Remove o elemento da frente da fila
+    print(f'O elemento da frente é: {filaTeste.peek()}')
+    filaTeste.remover()  # ............ Remove o elemento da frente da fila
+    print(f'{HEADER}Teste Nº 2 (Exclusões): {ENDC}')
+    print(f'{str(filaTeste.valor)} {OKBLUE} Tamanho da Fila: {filaTeste.tamanho()}{ENDC}\n')
+
+    # Testando Erro
+    print(f'{HEADER}Teste Nº 4 (Erro): {ENDC}')
+    filaTeste.remover()
+    filaTeste.remover()
+    filaTeste.remover()
+    filaTeste.remover()
+    filaTeste.remover()
+    print(f'{filaTeste.valor} {OKBLUE} Tamanho da fila: {filaTeste.tamanho()} {ENDC}')
+
+    # Verificando se a fila está vazia ou não
+    if filaTeste.estaVazia():
+        print('A fila está vazia!')
+    else:
+        print('A fila não está vazia!')
--- codigo_fonte/estruturas_de_dados/pilha.py
+++ codigo_fonte/estruturas_de_dados/pilha.py
@@ -1,73 +1,93 @@
+__author__ = ["Caio Henriques Sica Lamas",
+              "Everton Sousa Oliveira"]
+__date__ = "13/09/2022"
+__credits__ = ["https://www.techiedelight.com/pt/stack-implementation-python/"]
+__license__ = "GPL"
+__email__ = "eso@aluno.ifnmg.edu.br, caio.lamas@ifnmg.edu.br"
+
+from codigo_fonte.utilidades.utilidades import *
+
 # Implementação de Pilha em Python
 class Pilha:
 
-    # Construtor # para inicializar a Stack
+    """Construtor para inicializar a Pilha"""
     def __init__(self, tamanho):
+        self.valor = None
         self.arr = [None] * tamanho
         self.capacidade = tamanho
         self.topo = -1
 
-    # Função para adicionar um elemento `valor` à Stack
-    def push(self, valor):
-        if self.isFull():
-            print('Stack Overflow!! Calling exit()…')
+    """Função para adicionar um elemento valor à Pilha"""
+    def inserir(self, valor):
+        if self.estaCheio():
+            print('Pilha cheia! Encerrando processo...')
             exit(-1)
 
-        print(f'Inserindo {valor} na pilha…')
+        print(f'Inserindo {valor} na pilha...')
         self.topo = self.topo + 1
         self.arr[self.topo] = valor
 
-    # Função para remover um elemento superior da Stack
-    def pop(self):
-        # verifica se há underflow da Stack
+    """Função para remover um elemento superior da Pilha"""
+    def remover(self):
+        # verifica se a pilha está vazia
         if self.estaVazia():
-            print('Stack Underflow!! Calling exit()…')
+            print('Pilha vazia! Encerrando processo...')
             exit(-1)
 
         print(f'Removendo {self.peek()} da pilha')
 
-        # diminui o tamanho da Stack em 1 e (opcionalmente) retorna o elemento estourado
+        """Diminui o tamanho da Pilha em 1 e (opcionalmente) retorna o elemento estourado"""
         topo = self.arr[self.topo]
         self.topo = self.topo - 1
         return topo
 
-    # Função para retornar o elemento do topo da Stack
+    """Função para retornar o elemento do topo da Pilha"""
     def peek(self):
         if self.estaVazia():
             exit(-1)
         return self.arr[self.topo]
 
-    # Função para retornar o tamanho da Stack
+    """Função para retornar o tamanho da Pilha"""
     def tamanho(self):
         return self.topo + 1
 
-    # Função para verificar se a Stack está vazia ou não
+    """Função para verificar se a Pilha está vazia ou não"""
     def estaVazia(self):
         return self.tamanho() == 0
 
-    # Função para verificar se a Stack está cheia ou não
-    def isFull(self):
+    """Função para verificar se a Pilha está cheia ou não"""
+    def estaCheio(self):
         return self.tamanho() == self.capacidade
 
 
 if __name__ == '__main__':
 
-    pilha = Pilha(3)
-
-    pilha.push(1)  # Inserindo 1 na Stack
-    pilha.push(2)  # Inserindo 2 na Stack
-
-    pilha.pop()  # removendo o elemento superior (2)
-    pilha.pop()  # removendo o elemento superior (1)
-
-    pilha.push(3)  # Inserindo 3 na Stack
-
+    pilha = Pilha(3)  # ............ Define o tamanho da Pilha
+    #Testando inserção
+    pilha.inserir(1)  # ............ Inserindo 1 na Pilha
+    pilha.inserir(2)  # ............ Inserindo 2 na Pilha
+    pilha.inserir(3)  # ............ Inserindo 3 na Pilha
     print(f'O elemento do topo da pilha é {pilha.peek()}')
     print(f'O tamanho da pilha é {pilha.tamanho()}')
+    print(f'{HEADER}Teste Nº 1 (Inserções): {ENDC}')
+    print(f'{str(pilha.valor)} {OKBLUE} Tamanho da Fila: {pilha.tamanho()}{ENDC}\n')
 
-    pilha.pop()  # removendo o elemento superior (3)
+    #Testando remoção
+    pilha.remover()  # ............ Removendo o elemento superior (3)
+    pilha.remover()  # ............ Removendo o elemento superior (2)
+    print(f'{HEADER}Teste Nº 2 (Exclusões): {ENDC}')
+    print(f'{str(pilha.valor)} {OKBLUE} Tamanho da Fila: {pilha.tamanho()}{ENDC}\n')
 
-    # verifica se a Stack está vazia
+    # Testando Erro
+    print(f'{HEADER}Teste Nº 4 (Erro): {ENDC}')
+    pilha.remover()
+    pilha.remover()
+    pilha.remover()
+    pilha.remover()
+    pilha.remover()
+    print(f'{pilha.valor} {OKBLUE} Tamanho da fila: {pilha.tamanho()} {ENDC}')
+
+    # Verificando se a Pilha está vazia ou não
     if pilha.estaVazia():
         print('Essa pilha está vazia!')
     else:
