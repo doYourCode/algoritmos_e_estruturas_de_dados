1011e4709dedf1b402e115092db89004f35afb18
Everton Sousa Oliveira <evertonsousa3557@gmail.com>
2022-09-28 22:48:01 -0300

    Documentação das estruturas fila, fila_dupla, pilha e implementação de arvore_binaria

--- codigo_fonte/estruturas_de_dados/pilha.py
+++ codigo_fonte/estruturas_de_dados/pilha.py
@@ -9,7 +9,6 @@ from codigo_fonte.utilidades.utilidades import *
 
 # Implementação de Pilha em Python
 class Pilha:
-
     """Construtor para inicializar a Pilha"""
     def __init__(self, tamanho):
         self.valores = None
@@ -17,46 +16,59 @@ class Pilha:
         self.capacidade = tamanho
         self.topo = -1
 
-    """Função para adicionar um elemento valor à Pilha"""
     def inserir(self, valores):
-        if self.estaCheio():
-            print('Pilha cheia! Encerrando processo...')
-            exit(-1)
+        """
+        Função para adicionar um elemento valor à Pilha
 
+        :param valor: Informação que será armazenada
+        """
+        if self.estaCheio():
+            print(f"{FAIL} A fila está cheia, nenhuma operação foi realizada!{ENDC}")
+            exit(-1)
         print(f'Inserindo {valores} na pilha...')
         self.topo = self.topo + 1
         self.arr[self.topo] = valores
 
-    """Função para remover um elemento superior da Pilha"""
     def remover(self):
-        # verifica se a pilha está vazia
+        """
+        Função para remover um elemento superior da Pilha
+
+        :param valor: Informação que será removida
+        """
         if self.estaVazia():
-            print('Pilha vazia! Encerrando processo...')
+            print(f"{FAIL} Não há nenhum elemento na fila, nenhuma operação foi realizada!{ENDC}")
             exit(-1)
-
         print(f'Removendo {self.ler_topo()} da pilha')
-
         """Diminui o tamanho da Pilha em 1 e (opcionalmente) retorna o elemento estourado"""
         topo = self.arr[self.topo]
         self.topo = self.topo - 1
         return topo
 
-    """Função para retornar o elemento do topo da Pilha"""
+
     def ler_topo(self):
+        """
+        Função para retornar o elemento da frente da pilha sem apagar
+
+        :return: Informação do topo da fila
+        """
         if self.estaVazia():
             exit(-1)
         return self.arr[self.topo]
 
-    """Função para retornar o tamanho da Pilha"""
     def tamanho(self):
+        """
+        Função para retornar o tamanho da pilha
+
+        :return: Tamanho da pilha
+        """
         return self.topo + 1
 
-    """Função para verificar se a Pilha está vazia ou não"""
     def estaVazia(self):
+        """Função para verificar se a Pilha está vazia ou não"""
         return self.tamanho() == 0
 
-    """Função para verificar se a Pilha está cheia ou não"""
     def estaCheio(self):
+        """Função para verificar se a Pilha está cheia ou não"""
         return self.tamanho() == self.capacidade
 
 
