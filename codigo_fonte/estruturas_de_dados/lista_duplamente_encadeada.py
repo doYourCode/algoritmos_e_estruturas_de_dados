8c14228d1ad0dd12fe304a98fd6b2d80f457db03
franc <franckallyson@hotmail.com>
2022-10-28 18:37:38 -0300

    Atualização da Documentação da estrutura de dados lista_duplamente_encadeada.py

--- codigo_fonte/estruturas_de_dados/lista_duplamente_encadeada.py
+++ codigo_fonte/estruturas_de_dados/lista_duplamente_encadeada.py
@@ -4,8 +4,8 @@ Uma lista duplamente encadeada se comportará como uma lista, com a exceção de
 em dois sentidos, pois, em seus nodos, guarda referências para os próximos elementos, assim como os anteriores.
 """
 
-__author__ = ["Franck Allyson da Silva Rocha",
-              "Caio Henriques Sica Lamas"]
+__author__ = ["Caio Henriques Sica Lamas",
+              "Franck Allyson da Silva Rocha"]
 __date__ = "16/08/2022"
 __credits__ = ["https://www.tutorialspoint.com/python_data_structure/python_advanced_linked_list.htm",
                "https://www.geeksforgeeks.org/doubly-linked-list/",
@@ -20,8 +20,12 @@ from codigo_fonte.utilidades.utilidades import *
 
 class Nodo:
     """ Representa um único nó de uma lista """
-
     def __init__(self, valor=None):
+        """
+        Construtor da estrutura Nodo
+
+        :param valor: Valor que vai ser armazenado no Nó.
+        """
         self.valor = valor
         self.proximo = None
         self.anterior = None
@@ -34,16 +38,26 @@ class ListaDuplamenteEncadeada:
     """
 
     def __init__(self):
+        """Construtor da estrutura ListaDuplamenteEncadeada"""
         self.cabeca = None
         self._tamanho = 0
 
     @property
     def valores(self):
-        """ Conjunto dos nós. """
+        """
+        Propriedade da estrutura de dados
+
+        :return: Lista com o valor armazenado em cada nó.
+        """
         return [nodo.valor for nodo in self]
 
     def adicionar_inicio(self, valor):
-        """ Adiciona um novo nó no início da lista. """
+        """
+        Adiciona um novo nó no início da lista.
+
+        :param valor: Valor que será armazenado
+        :return:
+        """
         novo_nodo = Nodo(valor)
         novo_nodo.proximo = self.cabeca
         if self.cabeca is not None:
@@ -52,7 +66,12 @@ class ListaDuplamenteEncadeada:
         self._tamanho += 1
 
     def adicionar_final(self, valor):
-        """ Adiciona um novo nó ao final da lista. """
+        """
+        Adiciona um novo nó ao final da lista.
+
+        :param valor: Valor que será armazenado
+        :return:
+        """
         novo_nodo = Nodo(valor)
         novo_nodo.proximo = None
         if self.cabeca is None:
@@ -68,7 +87,13 @@ class ListaDuplamenteEncadeada:
         self._tamanho += 1
 
     def adicionar_pos(self, pos_nodo_n, valor):
-        """ Adiciona um novo nó em uma posição posterior à outro nó específico. """
+        """
+        Adiciona um novo nó em uma posição posterior à outro nó específico.
+
+        :param pos_nodo_n: Posição na estrutura
+        :param valor: Valor que será armazenado
+        :return:
+        """
         pos_nodo = self.buscar(pos_nodo_n)
         if pos_nodo is None:
             self._tamanho += 1
@@ -82,7 +107,12 @@ class ListaDuplamenteEncadeada:
         self._tamanho += 1
 
     def remover(self, valor):
-        """Remove um nó específico"""
+        """
+        Remove um nó específico através do seu valor
+
+        :param valor: Valor do nó que deverá ser removido
+        :return:
+        """
         nodo_temp = self.cabeca
         # Se o nó a ser excluído for a cabeça da lista
         if nodo_temp is not None and nodo_temp.valor == valor:
@@ -99,22 +129,42 @@ class ListaDuplamenteEncadeada:
             self._tamanho -= 1
 
     def buscar(self, valor):
-        """ Busca um nó especificado. """
+        """
+        Busca um nó especificado.
+
+        :param valor: Valor do nó a ser procurado
+        :return: Nó encontrado | None caso não encontrar
+        """
         for nodo in self:
             if nodo.valor == valor:
                 return nodo
         return None
 
     def esta_vazia(self):
+        """
+        Verifica se a estrutura de dados está vazia
+
+        :return: True = Vazia | False = Não está vazia
+        """
         if self._tamanho <= 0:
             return True
         return False
 
     def __iter__(self):
+        """
+        Função que define a forma de percorrer a estrutura de dados
+
+        :return: Cada nó pertencente a estrutura
+        """
         nodo_atual = self.cabeca
         while nodo_atual:
             yield nodo_atual
             nodo_atual = nodo_atual.proximo
 
     def __len__(self):
+        """
+        Função que mostra o tamanho da estrutura
+
+        :return: Tamanho da estrutura
+        """
         return self._tamanho
