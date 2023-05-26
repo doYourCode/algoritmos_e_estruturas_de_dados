44b92f083cd57aad062a52a10f035fe11c55acc0
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2023-05-26 17:52:01 -0300

    Select Sort - Correção

--- codigo_fonte/ordenacao/selection_sort.py
+++ codigo_fonte/ordenacao/selection_sort.py
@@ -9,47 +9,17 @@ __author__ = ["Tomaz Martins Batista",
               "Jean Pereira Coelho",
               "Caio Henriques Sica Lamas"]
 __date__ = "22/05/2023"
-__credits__ = ["https://algoritmosempython.com.br/cursos/algoritmos-python/estruturas-dados/listas-encadeadas/",
-               "https://algoritmosempython.com.br/cursos/algoritmos-python/pesquisa-ordenacao/selecao/"]
+__credits__ = ["https://www.geeksforgeeks.org/selection-sort/"]
 __license__ = "GPL"
 __email__ = "tmb5@aluno.ifnmg.edu.br, jpc3@aluno.ifnmg.edu.br, caio.lamas@ifnmg.edu.br"
 
-
-from codigo_fonte.estruturas_de_dados.lista_encadeada import ListaEncadeada
-
-
-def selection_sort(lista):  # funçao Selection Sort
-    # Caso base
-    if lista.cabeca is None:
-        return None
-
-    # Inicialização de variáveis
-    atual = lista.cabeca
-    while atual.proximo:
-        nodo_minimo = atual
-        nodo_proximo = atual.proximo
-        while nodo_proximo:
-            if nodo_proximo.valor < nodo_minimo.valor:
-                nodo_minimo = nodo_proximo
-            nodo_proximo = nodo_proximo.proximo
-
-        # Trocar os nós correspondentes
-        atual.valor, nodo_minimo.valor = nodo_minimo.valor, atual.valor
-        atual = atual.proximo
-    return lista.cabeca
-
-# COMENTÁRIO DO PROFESSOR:
-# Esse trecho vai para o arquivo testes.py metodo especifico pro selection sort, olhar exemplo do bogosort
-minha_lista = ListaEncadeada()
-
-minha_lista.adicionar_final(4)
-minha_lista.adicionar_final(2)
-minha_lista.adicionar_final(1)
-minha_lista.adicionar_final(3)
-minha_lista.adicionar_final(5)
-
-print(minha_lista.valores)
-
-lista_ordenada = selection_sort(minha_lista)
-
-print(minha_lista.valores)
+def selection_sort(nums):
+    for i in range(len(nums)):
+        # Encontra o índice do menor elemento restante no subarray não ordenado
+        min_idx = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+        # Troca o elemento atual com o menor elemento encontrado
+        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+    return nums
--- codigo_fonte/ordenacao/testes.py
+++ codigo_fonte/ordenacao/testes.py
@@ -4,7 +4,9 @@ from codigo_fonte.ordenacao.bogo_sort import *
 # COMENTARIO DO PROFESSOR
 # observar como foram feitos os testes das EDs no repositório
 def teste_selection_sort():
-    # TODO
+    num1 = input('Input  comma separated numbers:\n').strip()
+    nums = [int(item) for item in num1.split(',')]
+    print(selection_sort(nums))
     pass
 
 def teste_bogo_sort():
