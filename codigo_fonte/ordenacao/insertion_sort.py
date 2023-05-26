76cd1749df53c76bafabfd24d4d6de3422f91fa5
José Rodrigues <josejunior170@hotmail.com>
2023-05-26 12:32:13 -0300

    Correções

--- codigo_fonte/ordenacao/insertion_sort.py
+++ codigo_fonte/ordenacao/insertion_sort.py
@@ -1,7 +1,7 @@
 """
-O método Insertion Sort lida com a ordenação: 
-Fazendo comparações que o menor número sempre vá para a esquerda.
-Não se inicia da primeira posição, pois a lista pode ter apenas um único elemento, então nesse caso a lista se inicia com 0.
+O método de ordenção Insertion Sort funciona: 
+Fazendo comparações em que o menor número sempre vá para a esquerda.
+Não se inicia da primeira posição, pois a lista pode ter apenas um único elemento, então ela já está ordenada.
 Possui a complexidade de:
     O(n) no melhor caso.
     O(n²) no caso médio e pior caso.
@@ -9,7 +9,7 @@ Sendo assim um método de ordenação estável.
 """
 
 __author__ = ["Caio César Oliveira Silva",
-              "José Rorigues Costa Júnior",
+              "José Rodrigues Costa Júnior",
               "Jhonatan Rotta Santana",
               "Anthony Henrique de Souza Guimarães"]
 __date__ = "26/05/2023"
@@ -20,11 +20,13 @@ __email__ = "ccos1@aluno.ifnmg.edu.br", "jrcj@aluno.ifnmg.edu.br", "jrs7@aluno.i
 def insertion_sort(lista):
     quantidade_elementos = len(lista)
     for i in range(1, quantidade_elementos):
-        atual = lista[i] # Pega a posição do elemento e começa do segundo item que compara com o anterior.
+        atual = lista[i] # Pega a posição do elemento atual.
         j = i - 1
-        while j >= 0 and lista[j] > atual:  # Se for menor que o anterior ele vai para trás.
-                                            # Se o anterior for maior ele, ele vai para frente.  
-                                            # Se for igual não fará nada.
+
+        # Compara o atual com cada elemento na esquerda dele, até que um elemento menor que ele seja encontrado.
+        # Se for encontrado um elemento maior que o atual, move o atual para a esquerda deste elemento.
+        # Se não for encontrado, continua na sua posição.
+        while j >= 0 and lista[j] > atual:  
             lista[j + 1] = lista[j] 
-            j = j - 1  
+            j = j - 1 
         lista[j + 1] = atual
