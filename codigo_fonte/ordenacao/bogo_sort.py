71af77cac971f50c998ca8365fcd479e76ab0c25
Caio Henriques Sica Lamas <111379877+doYourCode@users.noreply.github.com>
2023-06-06 23:13:52 -0300

    Update bogo_sort.py

--- codigo_fonte/ordenacao/bogo_sort.py
+++ codigo_fonte/ordenacao/bogo_sort.py
@@ -13,15 +13,22 @@ __email__ = "caio.lamas@ifnmg.edu.br"
 import random
 
 
-def bogo_sort(nums):
-    def is_sorted(nums):
-        if len(nums) < 2:
+def bogo_sort(lista):
+    """
+    Um dos algoritmos de ordenação mais ineficientes comumente demonstrado como exemplo de algoritmo de complexidade
+    que cresce de forma exponencial. Com menos de uma dezena de valores a ordenar seu tempo de execução pode ser
+    indeterminado.
+    :param lista O conjunto de dados que se pretende ordenar (pretende-se mesmo?
+    :return O conjunto ordenado, ou executa por tempo indeterminado
+    """
+    def is_sorted(lista):
+        if len(lista) < 2:
             return True
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
+        for i in range(len(lista) - 1):
+            if lista[i] > lista[i + 1]:
                 return False
         return True
 
-    while not is_sorted(nums):
-        random.shuffle(nums)
-    return nums
+    while not is_sorted(lista):
+        random.shuffle(lista)
+    return lista
