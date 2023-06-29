626ace92e246f23409116319f778e7cc4fb82f34
Caio Henriques Sica Lamas <111379877+doYourCode@users.noreply.github.com>
2023-06-29 10:47:14 -0300

    Create grafo_matriz_adjacencia.py

---
+++ codigo_fonte/estruturas_de_dados/grafo_matriz_adjacencia.py
@@ -0,0 +1,17 @@
+"""
+Grafos são estruturas de dados não-lineares, compreendendo um conjunto finito de vértices e arestas.
+Essa implementação utiliza um array bidimensional para guardar pesos e relações de vizinhança entre os nós.
+Aviso: Implementação de um grafo direcionado.
+"""
+__author__ = 'Caio Henriques Sica Lamas'
+__date__ = '29/06/2023'
+__credits__ = ''
+__license__ = 'GPL'
+__email__ = 'caio.lamas@ifnmg.edu.br'
+
+from codigo_fonte.utilidades.utilidades import *
+
+
+class GrafoListaAdjacencia:
+
+    pass
